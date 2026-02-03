ec15084c9bf6937a050d3fec03d9db30b62b3151
Yuvraj <uvgoyal882@gmail.com>
2026-02-03 14:51:16 +0530

    correct baseURL

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://driver-hire-7.onrender.com",
+      "https://driver-hire.onrender.com"
+    ],
     credentials: true,
   })
 );
